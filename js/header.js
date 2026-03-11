053717dd9f3138a480483ae2cf12ce98f0ca645e
ch <82921898+clean-teach@users.noreply.github.com>
2026-03-11 18:19:00 +0900

    Feat : "스크롤 위로 가기" 버튼 추가

--- js/header.js
+++ js/header.js
@@ -9,6 +9,8 @@
   const $siteMap = document.querySelector('#siteMap');
   /** 전체 메뉴 닫기 버튼 선택자 */
   const $totalMenuCloseButton = $siteMap.querySelector('.close-button');
+  /** 스크롤 위로 가기 버튼 선택자 */
+  const $scrollTopButton = document.querySelector('.scroll-top-button');
 
   /** 브라우저 전체 화면 스크롤 핸들러 */
   const handleScroll = (event) => {
@@ -31,5 +33,8 @@
   });
   $totalMenuCloseButton.addEventListener('click', () => {
     $siteMap.classList.remove('active');
+  });
+  $scrollTopButton.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   });
 })();
